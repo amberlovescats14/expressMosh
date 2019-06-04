9908bae52ba27d622d7136836cbd2bf5a21156b7
Amber Jones <amberlovescats14@gmail.com>
2019-06-04 17:24:51 -0500

    finished using Joi verification

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.get('/api/courses', (req, res) => {
 //! we can have multiple params like this, ('api/posts/:year/:month', (req, res))
 app.get('/api/courses/:id', (req, res) => {
   const course = courses.find(c => c.id === parseInt(req.params.id));
-  if(!course) res.status(404).send('The course with this id, was not found');
+  if(!course) return res.status(404).send('The course with this id, was not found');
   res.send(course)
   //res.send(req.params.id)
   //req.params.year
@@ -49,13 +49,13 @@ app.post('/api/courses', (req, res) => {
   //   return;
   // }
   //! using JOI
-  const schema = {
-    name: Joi.string().min(3).required()
-  }
-  const result = Joi.validate(req.body, schema)
-  if(result.error){
-    return res.status(400).send(result.error.details[0].message)
-  }
+  const { error } = validateCoarse(req.body)
+
+  // If coarse does not exist. Bad request. return 400
+  if(error) return res.status(400).send(result.error.details[0].message)
+  
+
+
   const course = {
     id: courses.length +1,
     name: req.body.name
@@ -68,28 +68,30 @@ app.post('/api/courses', (req, res) => {
 app.put('/api/courses/:id', (req, res)=> {
   // look up the course.
   const course = courses.find(c => c.id === parseInt(req.params.id));
+    if(!course) return res.status(404).send('The course with this id, was not found')
 
-  //create a constant for the validate function with schema
+  // bad request
   const { error } = validateCoarse(req.body)
+  if(error) return res.status(400).send(error.details[0].message)
   
 
-  // if not existing return 404. Resourse not found
-  if(error) res.status(404).send('The course with this id, was not found');
-
-  // If coarse does not exist. Bad request. return 400
-  const schema = {
-    name: Joi.string().min(3).required()
-  }
-  const result = Joi.validate(req.body, schema);
-  if(result.error){
-    return res.status(400).send(result.error.details[0].message)
-  }
-
-  // Validate to make sure the info is good.  return updated info
-  ConstantSourceNode.name = req.body.name;
-  res.send(coarse)
+  // Validate. If the coarse does exist. Update it
+  course.name = req.body.name;
+  res.send(course)
 })
 
+
+app.delete('/api/courses/:id', (req, res) => {
+  // 1. look up the coarse, if it doesnt exist, give it a 404
+  const course = courses.find(c => c.id === parseInt(req.params.id));
+  if(!course) return res.status(404).send('The course with this id, was not found');
+
+  //2. if it does exist, delete it and return the other coarses
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+
+  return res.send(course)
+})
 const validateCoarse = (course) => {
   const schema = {
     name: Joi.string().min(3).required()
